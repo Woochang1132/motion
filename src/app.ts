b8d30ccad3f7be5d7930a04936490dbbf0372551
Woochang1132 <show4031132@gmail.com>
2023-11-27 15:35:40 +0900

    typeScript => videoComponent 구성

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ImageComponent } from './components/item/image.js';
 import { NoteComponent } from './components/item/note.js';
 import { TodoComponent } from './components/item/todo.js';
+import { VideoComponent } from './components/item/video.js';
 import { PageComponent } from './components/page/page.js';
 
 class App{
@@ -11,6 +12,9 @@ class App{
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/seed/picsum/200/300')
         image.attachTo(appRoot,'beforeend');
+
+        const video = new VideoComponent('Video Title', 'https://youtu.be/DXB84VgBP18?t=8');
+        video.attachTo(appRoot, 'beforeend')
 
         const note = new NoteComponent('note Title', 'Note Body')
         note.attachTo(appRoot,'beforeend');
---
+++ src/components/item/video.ts
@@ -0,0 +1,36 @@
+import { BaseComponent } from '../components.js'
+
+
+export class VideoComponent extends BaseComponent<HTMLElement>{
+    constructor(title : string, src : string){
+        super(`
+        <section class='video'>
+            <div class='video__player'>
+                <iframe src=""class="video__iframe"></iframe>
+                <h3 class="video__title"></h3>
+            </div>
+        </section>`
+    );
+
+    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+    titleElement.textContent = title;
+
+    console.log(src)
+    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+    iframe.src = this.covertToEmbededURL(src); // url => videoId로 변환해야 한다.
+    }
+
+    private covertToEmbededURL(url: string) : string{
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+        console.log("match >>" , match)
+
+        const videoId = match ? match[1] || match[2] : undefined;
+
+        if(videoId){
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+        return url;
+    }
+
+}
