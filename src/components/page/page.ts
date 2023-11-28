ade561f0364bc28c892e6e7b2bad3d6b47c486ae
Woochang1132 <show4031132@gmail.com>
2023-11-28 16:25:09 +0900

    typeScript => pageComponent의 구조 변경

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,7 +1,34 @@
-import { BaseComponent } from '../components.js';
+import { BaseComponent, Component } from '../components.js';
 
-export class PageComponent extends BaseComponent<HTMLUListElement>{
+export interface Composable{
+    addChild(child : Component) : void
+}
+
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable{
+    constructor(){
+        super(`
+            <li class="page-item">
+                <section class="page-item__body">
+                    <div class="page-item__controals">
+                        <button class="close">&times;</button>
+                    </div>
+                </section>
+            </li>`);
+    }
+    addChild(child: Component){
+        const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+        child.attachTo(container);    
+    }
+}
+
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable{
     constructor() {
-        super('<ul class="page">This is PageComponent</ul>')
+        super('<ul class="page"></ul>')
+    }
+
+    addChild(section: Component){
+        const item = new PageItemComponent();
+        item.addChild(section);
+        item.attachTo(this.element, 'beforeend');
     }
 }
