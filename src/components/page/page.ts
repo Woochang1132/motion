2daf9da94fa42df9c8a9c4b1e9658a27bebd0743
Woochang1132 <show4031132@gmail.com>
2023-11-29 13:33:35 +0900

    typeScript => removeChild 활용해서 삭제하기

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,8 +3,10 @@ import { BaseComponent, Component } from '../components.js';
 export interface Composable{
     addChild(child : Component) : void
 }
+type OnCloseListener = () => void;
 
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable{
+    private closeListener?: OnCloseListener;
     constructor(){
         super(`
             <li class="page-item">
@@ -14,10 +16,17 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
                     </div>
                 </section>
             </li>`);
+            const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+            closeBtn.onclick = () => {
+                this.closeListener && this.closeListener();
+            }
     }
     addChild(child: Component){
         const container = this.element.querySelector('.page-item__body')! as HTMLElement;
         child.attachTo(container);    
+    }
+    setOnCloseListener(listener : OnCloseListener) {
+        this.closeListener = listener;
     }
 }
 
@@ -30,5 +39,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
         const item = new PageItemComponent();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
+        item.setOnCloseListener(() => {
+            item.removeFrom(this.element);
+        })
     }
 }
