458a547191411113a8e7cf2cb7fac9239d42b9c5
Woochang1132 <show4031132@gmail.com>
2023-12-01 16:41:17 +0900

    Motion => title태크에 class명 추가

--- src/components/item/image.ts
+++ src/components/item/image.ts
@@ -8,7 +8,7 @@ export class ImageComponent extends BaseComponent<HTMLElement>{
                 <div class="image__holder">
                     <img src="" alt="" class="image__thumbnail">
                 </div>
-                <h2 class="image__title"></h2>
+                <h2 class="page-item__title image__title"></h2>
             </section>
             `)
 
